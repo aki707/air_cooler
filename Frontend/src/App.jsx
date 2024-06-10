f4367adddd1704d7580a5dbf7649859dc10d5283
Akshat-44 <akshat44aj@gmail.com>
2024-06-10 12:24:00 +0530

    product page changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from './components/DevicesPage/Dashboard.jsx';
 // import Navbar from './components/DevicesPage/Navbar.jsx';
 import Loginspage from './components/Login/Loginspage.jsx';
+import ProductPage from './components/ProductsPage/ProductPage.jsx';
+import ProductTab from './components/ProductsPage/ProductTab';
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={Loginspage} />
       <Route path="/dashboard" Component={Dashboard} />
+      <Route path="/pg" Component={ProductPage} />
+      <Route path="/pt" Component={ProductTab} />
+
+
 
       {/* <Route path='/buy' Component={Buy}/> */}
     </Routes>
