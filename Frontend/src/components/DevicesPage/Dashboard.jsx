305f38e543d69b5508e79e6297aa65aac642f4ed
Akshat-44 <akshat44aj@gmail.com>
2024-06-10 10:01:09 +0530

    dashboard changes

--- Frontend/src/components/DevicesPage/Dashboard.jsx
+++ Frontend/src/components/DevicesPage/Dashboard.jsx
@@ -1,20 +1,7 @@
 import {
-  Activity,
   ArrowUpRight,
-  CircleUser,
-  CreditCard,
   DollarSign,
-  Menu,
-  Package2,
-  Search,
-  Users,
 } from "lucide-react"
-
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
@@ -24,8 +11,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Switch } from "@/components/ui/switch"
 import {
   Table,
   TableBody,
