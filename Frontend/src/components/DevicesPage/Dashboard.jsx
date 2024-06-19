508eca7d6b37eba24656d05da2eac454563c6d6c
Akshat-44 <akshat44aj@gmail.com>
2024-06-19 11:32:08 +0530

    Options Updated

--- Frontend/src/components/DevicesPage/Dashboard.jsx
+++ Frontend/src/components/DevicesPage/Dashboard.jsx
@@ -1,4 +1,13 @@
 import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
  Plus,
  Search,
 } from "lucide-react"
@@ -215,7 +224,90 @@ const handleEdit = async (id, value) => {
                     <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
                       2023-06-23
                     </TableCell>
-                    <TableCell className="text-right"> <Options /> </TableCell>
+                    <TableCell className="text-right">     <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button className="threedots">
+                      <h6 className="threedottext">
+                        ...
+                      </h6>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    
+                    <div>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button asChild size="sm" className="ml-auto gap-1">
+                            <a href="#">
+                              Edit
+                            </a>
+                          </Button>
+                        </DialogTrigger>
+
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle>
+                              Edit Device Name
+                            </DialogTitle>
+                            {/* <DialogDescription>
+                              You can find device name on website or on the device itself.
+                            </DialogDescription> */}
+                          </DialogHeader>
+                          <div className='grid gap-4 py-4'>
+                            {/* <div className='grid gap-2'>
+                               New Device Name */}
+                              <div className="relative">
+                                  <DialogDescription className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                                    <Input
+                                    type="DropdownMenuSeparatorch"
+                                    placeholder="Enter New Name"
+                                    className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                                    />
+                              </div>
+                {/* <Label htmlFor='url'>   Device Name</Label> */}
+                            {/* </div> */}
+                          </div>
+                          <DialogFooter>
+                            <Button>
+                              Update
+                            </Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+
+                    </div>
+                    <div>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                          <Button asChild size="sm" className="ml-auto gap-1">
+                            <a href="#">
+                              Delete
+                            </a>
+                          </Button>
+                        </DialogTrigger>
+
+                        <DialogContent>
+                          <div className='grid gap-4 py-4'>
+                            <div className='grid gap-2'>
+                            Are You Sure You Want To Remove This Device?
+                {/* <Label htmlFor='url'>   Device Name</Label> */}
+                            </div>
+                          </div>
+                          <DialogFooter>
+                            <Button onClick={()=> handleDelete(data.id)}>
+                              Delete
+                            </Button>
+                            <Button>
+                              Cancel
+                            </Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+
+                    </div>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                 </TableCell>
                   </TableRow>
      
                 )  })}</>}
